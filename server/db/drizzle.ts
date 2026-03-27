4dbb9306e32585557a28b7844a5f8fa5072d80b1
HAL <68320771+HALQME@users.noreply.github.com>
2026-03-27 10:32:13 +0900

    update

--- server/db/drizzle.ts
+++ server/db/drizzle.ts
@@ -1,10 +1,12 @@
+import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql/web';
 
 export const dbClient = (env: Env) => {
+  const client = createClient({
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  });
   return drizzle({
-    connection: {
-      url: env.TURSO_DATABASE_URL,
-      authToken: env.TURSO_AUTH_TOKEN,
-    },
+    client,
   });
 };

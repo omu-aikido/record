620a6279178a2c703eaf1f5af82196214f07f7b1
HAL <68320771+halqme@users.noreply.github.com>
2026-09-08 00:05:00 +0900

    Enable Vue Query Devtools in development

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
 import AppFooter from "./components/pages/AppFooter.vue";
 import AppHeader from "./components/pages/AppHeader.vue";
 import ErrorBoundary from "./components/common/ErrorBoundary.vue";
+
+const isDev = import.meta.env.DEV;
 </script>
 
 <template>
@@ -11,5 +14,6 @@ import ErrorBoundary from "./components/common/ErrorBoundary.vue";
       <RouterView />
     </main>
     <AppFooter />
+    <VueQueryDevtools v-if="isDev" />
   </ErrorBoundary>
 </template>
